2820818dbe7677589fd4421dc37e28c0223765cb
Eduardo Turiño <eturino@eturino.com>
2021-01-26 12:24:07 +0100

    chore(typedoc): update doc generation to new typedoc

--- typedoc.js
+++ typedoc.js
@@ -1,16 +1,8 @@
 module.exports = {
   out: "build/docs",
 
-  exclude: [
-    "**/__tests__/**/*",
-    "**/__test_utils__/**/*",
-    "**/__fixtures__/**/*",
-    "**/*.spec.ts"
-  ],
+  exclude: ["**/__tests__/**/*", "**/__test_utils__/**/*", "**/__fixtures__/**/*", "**/*.spec.ts"],
 
-  target: "ES6",
   excludeExternals: true,
-  excludeNotExported: true,
   excludePrivate: true,
-  mode: "file"
 };
