9a1678e479fbb00900c60a55ca3242b13aa4d19c
Eduardo Turiño <eturino@eturino.com>
2021-09-13 18:29:50 +0200

    chore: update typedoc config

--- typedoc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  out: "build/docs",
-
-  exclude: ["**/__tests__/**/*", "**/__test_utils__/**/*", "**/__fixtures__/**/*", "**/*.spec.ts"],
-
-  excludeExternals: true,
-  excludePrivate: true,
-};
